f9439d62731a772620cfd66ff541a0e575fca1e6
Ian Wigley <ian.wigley@outlook.com>
2023-07-30 18:04:27 +0100

    Delete old folder

--- Platformer/Platformer/Platformer/MathHelper.ts
+++
@@ -1,9 +0,0 @@
-﻿module Platformer1 {
-    export class MathHelper {
-        public static Clamp(value: number, min: number, max: number): number {
-            value = (value > max) ? max : value;
-            value = (value < min) ? min : value;
-            return value;
-        }
-    }
-}
