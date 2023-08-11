d23b3bfe9bda746c137b2306894f6206440d1017
Ian Wigley <ian.wigley@outlook.com>
2023-08-11 19:06:22 +0100

    Create mathhelper.ts

---
+++ Platformer/Platformer-VSCode/ts/mathhelper.ts
@@ -0,0 +1,7 @@
+﻿export class MathHelper {
+    public static Clamp(value: number, min: number, max: number): number {
+        value = (value > max) ? max : value;
+        value = (value < min) ? min : value;
+        return value;
+    }
+}
